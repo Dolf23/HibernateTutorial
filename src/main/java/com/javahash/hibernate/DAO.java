fca41531ec26046c8f3ef13baac6ee410aede90a
Dolf <dmitry.dailidovich@gmail.com>
2016-05-30 16:22:28 +0300

    dao add

---
+++ src/main/java/com/javahash/hibernate/DAO.java
@@ -0,0 +1,61 @@
+package com.javahash.hibernate;
+
+import org.hibernate.Session;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+public class DAO {
+    private Session session;
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String name;
+
+    public void add() throws IOException {
+        session = HibernateSessionManager.getSessionFactory().openSession();
+        session.beginTransaction();
+        User user = new User();
+        System.out.print("Enter name:\n");
+        name = reader.readLine();
+        user.setUsername(name);
+        user.setCreatedBy("Application");
+        user.setCreatedDate(new Date());
+
+        session.save(user);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(int id) throws IOException {
+        session = HibernateSessionManager.getSessionFactory().openSession();
+        session.beginTransaction();
+        User user = (User) session.get(User.class, id);
+
+        session.delete(user);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void update(int id) throws IOException {
+        session = HibernateSessionManager.getSessionFactory().openSession();
+        session.beginTransaction();
+        User user = (User) session.get(User.class, id);
+        System.out.print("Enter new name:\n");
+        name = reader.readLine();
+        user.setUsername(name);
+
+        session.saveOrUpdate(user);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void show(int id) throws IOException {
+        session = HibernateSessionManager.getSessionFactory().openSession();
+        session.beginTransaction();
+        User user = (User) session.get(User.class, id);
+        String userInformation = user.getUserId() + " " + user.getUsername() + " " + user.getCreatedBy() + " " + user.getCreatedDate() + ";";
+        System.out.println(userInformation);
+        session.close();
+    }
+}
--- src/main/java/com/javahash/hibernate/Run.java
+++ src/main/java/com/javahash/hibernate/Run.java
@@ -1,57 +1,61 @@
 package com.javahash.hibernate;
 
-import org.hibernate.Session;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Date;
 
 public class Run {
 
     /**
      * @param args
      */
-    private Session session = HibernateSessionManager.getSessionFactory().openSession();
-
     public static void main(String[] args) throws IOException {
         String menu = "Please enter the following command:\n" +
-                "1. User add.\n" +
-                "2. User update.\n" +
-                "3. User delete.\n" +
-                "4. Exit.";
+                "1. Add user.\n" +
+                "2. Update user.\n" +
+                "3. Delete.\n" +
+                "4. Show user.\n" +
+                "5. Exit.";
+        String enterId = "Enter ID:";
         System.out.println("Hello.\n");
         System.out.println(menu);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String rLine = reader.readLine();
+        String command = reader.readLine().trim();
+        DAO dao = new DAO();
 
-        while (!rLine.equals("4")){
-            switch (rLine)
+        while (!command.equals("5")) {
+            switch (command) {
+                case "1":{
+                    dao.add();
+                }
+                    break;
+                case "2":{
+                    System.out.print(enterId);
+                    int id = Integer.parseInt(reader.readLine().trim());
+                    dao.update(id);
+                }
+                    break;
+                case "3":{
+                    System.out.print(enterId);
+                    int id = Integer.parseInt(reader.readLine().trim());
+                    dao.delete(id);
+                }
+                    break;
+                case "4":{
+                    System.out.print(enterId);
+                    int id = Integer.parseInt(reader.readLine().trim());
+                    dao.show(id);
+                }
+                    break;
+                default: {
+                    System.out.println("Incorrect command.");
+                }
+            }
+            System.out.println(menu);
+            command = reader.readLine().trim();
         }
 
-    }
-
-    private void add(){
-        session.beginTransaction();
-        User user = new User();
-
-        System.out.println("Enter name:\n");
-        user.setUserId(3);
-        user.setUsername("Chan");
-        user.setCreatedBy("Application");
-        user.setCreatedDate(new Date());
-
-        session.save(user);
-        session.getTransaction().commit();
-    }
-
-    private  void delete(){
-        session.beginTransaction();
-        User user = new User();
-
-    }
-
-    private void updete(){
+        System.out.println("Good Bye!");
 
     }
 
